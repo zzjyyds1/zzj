3b18499c609713d2260c71cb297d3eb076e9c159
zzjyyds1 <2145087604@qq.com>
2023-11-15 21:28:32 +0800

    test

---
+++ zzj朱正杰/zzj朱正杰.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int k(int s ) {
+	cout << s << endl;
+	return s;
+}
+int main()
+{
+	int a = 20, b = 10, s=100000;
+	cout << max(a,b);
+	int c = k(100);	cout << s;
+
+
+}
